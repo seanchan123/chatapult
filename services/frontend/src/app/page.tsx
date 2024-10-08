c298e49488d18857107e336bfea52aeba5892086
Sean Chan <seanchan@outlook.sg>
2024-10-08 19:39:58 +0800

    Updated aesthetics of `/`

--- services/frontend/src/app/page.tsx
+++ services/frontend/src/app/page.tsx
@@ -7,15 +7,16 @@ const Home: React.FC = () => {
   return (
     <div className="h-screen bg-white dark:bg-gray-800 color-transition">
       {/* Hero Section */}
-      <section className="w-full h-5/6 shadow-sm flex items-center justify-center bg-indigo-400 dark:bg-indigo-950 color-transition">
-        <div className="text-center w-full sm:w-2/3 md:w-1/2 lg:w-1/3 -mt-12 sm:-mt-16 md:-mt-24">
-          <div className="italic text-3xl sm:text-4xl md:text-5xl dark:text-white">
+      <section className="w-full h-1/2 shadow-sm flex items-center justify-center bg-indigo-400 dark:bg-indigo-950 color-transition">
+        <div className="text-center w-full sm:w-2/3 md:w-1/2 lg:w-1/3 -mt-4 sm:-mt-6 md:-mt-10">
+          <div className="italic text-3xl sm:text-4xl md:text-5xl text-gray-800 dark:text-white">
             Launch your skills with AI dialogue using{" "}
-            <span className="font-black text-indigo-700 dark:text-indigo-500">
-              Chatapult
+            <span className="font-black bg-gradient-to-t bg-clip-text text-transparent
+            from-indigo-500 to-indigo-800 dark:from-indigo-300 dark:to-indigo-500">
+              Chatapult.
             </span>
           </div>
-          <div className="mt-2 text-base sm:text-lg md:text-xl dark:text-gray-300">
+          <div className="mt-2 font-light text-base sm:text-lg md:text-xl dark:text-gray-300">
             The ultimate AI chatbot platform
           </div>
 
