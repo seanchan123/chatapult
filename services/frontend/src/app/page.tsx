00c86f334711c0951f426f6e26f51d1e647b60d6
Sean Chan <seanchan@outlook.sg>
2024-10-08 22:26:04 +0800

    Re-center hero section in `/`

--- services/frontend/src/app/page.tsx
+++ services/frontend/src/app/page.tsx
@@ -7,12 +7,14 @@ const Home: React.FC = () => {
   return (
     <div className="pt-24 h-screen bg-white dark:bg-gray-800 color-transition">
       {/* Hero Section */}
-      <section className=" w-full h-1/2 shadow-sm flex items-center justify-center bg-indigo-400 dark:bg-indigo-950 color-transition">
-        <div className="text-center w-full sm:w-2/3 md:w-1/2 lg:w-1/3 -mt-4 sm:-mt-6 md:-mt-10">
+      <section className="w-full h-1/2 shadow-sm flex items-center justify-center bg-indigo-400 dark:bg-indigo-950 color-transition">
+        <div className="text-center w-full sm:w-2/3 md:w-1/2 lg:w-1/3 -mt-4 sm:-mt-4 md:-mt-6">
           <div className="italic text-3xl sm:text-4xl md:text-5xl text-gray-800 dark:text-white">
             Launch your skills with AI dialogue using{" "}
-            <span className="font-black bg-gradient-to-t bg-clip-text text-transparent
-            from-indigo-500 to-indigo-800 dark:from-indigo-300 dark:to-indigo-500">
+            <span
+              className="font-black bg-gradient-to-t bg-clip-text text-transparent
+            from-indigo-500 to-indigo-800 dark:from-indigo-300 dark:to-indigo-500"
+            >
               Chatapult.
             </span>
           </div>
