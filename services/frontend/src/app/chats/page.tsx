fc9aecac9ae3284bcbdec9acbdb38c445085eb2f
Sean Chan <seanchan@outlook.sg>
2024-10-12 11:52:08 +0800

    Created base `/chats` page

---
+++ services/frontend/src/app/chats/page.tsx
@@ -0,0 +1,17 @@
+// src/app/chats/page.tsx
+"use client";
+
+import React from "react";
+
+const ChatsPage: React.FC = () => {
+
+  return (
+    <div className="h-screen py-12 px-20">
+      <div className=" flex flex-col space-y-8 mt-28">
+       
+      </div>
+    </div>
+  );
+};
+
+export default ChatsPage;
