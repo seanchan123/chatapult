d55dedad7ffedb05e8e745e6649252b07895b1aa
Sean Chan <seanchan@outlook.sg>
2024-10-13 17:48:45 +0800

    Fixed background of `/chats`

--- services/frontend/src/app/chats/page.tsx
+++ services/frontend/src/app/chats/page.tsx
@@ -122,7 +122,7 @@ const ChatsPage: React.FC = () => {
   };
 
   return (
-    <div className="py-8 px-4 md:px-20">
+    <div className="min-h-screen py-8 px-4 md:px-20">
       <div className="flex flex-col space-y-8 mt-10 md:mt-28">
         {/* Actions Section */}
         <div>
