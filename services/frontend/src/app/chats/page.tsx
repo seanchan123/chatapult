143702e545fc5d0837c0a9de8e866965524c97e1
Sean Chan <seanchan@outlook.sg>
2024-10-13 17:25:24 +0800

    Added section for chats in `/chats`

--- services/frontend/src/app/chats/page.tsx
+++ services/frontend/src/app/chats/page.tsx
@@ -4,31 +4,120 @@
 import Link from "next/link";
 import React, { useState } from "react";
 
+interface Chat {
+  id: string;
+  name: string;
+  date: string;
+  tags: string[];
+}
+
 interface Folder {
   id: string;
   name: string;
-  files: string[];
+  chats: string[];
 }
 
 const ChatsPage: React.FC = () => {
   const [search, setSearch] = useState("");
   const [folders, setFolders] = useState<Folder[]>([
-    { id: "1", name: "Machine Learning", files: [] },
-    { id: "2", name: "Data Analytics", files: [] },
+    { id: "1", name: "Machine Learning", chats: [] },
+    { id: "2", name: "Data Analytics", chats: [] },
+  ]);
+  const [chats, setChats] = useState<Chat[]>([
+    {
+      id: "1",
+      name: "Jon Jones",
+      date: "19/07/1987",
+      tags: ["Bones", "Drugs", "GOAT"],
+    },
+    {
+      id: "2",
+      name: "Alex Pereira",
+      date: "07/07/1987",
+      tags: ["Poatan", "Left-hook", "Beer"],
+    },
+    {
+      id: "3",
+      name: "Charles Oliveira",
+      date: "17/10/1989",
+      tags: ["Cowboy", "Skull", "BJJ"],
+    },
+    {
+      id: "4",
+      name: "Kamaru Usman",
+      date: "11/05/1987",
+      tags: ["Nigerian Nightmare", "Wrestling", "Champ"],
+    },
+    {
+      id: "5",
+      name: "Israel Adesanya",
+      date: "22/07/1989",
+      tags: ["Stylebender", "Kickboxing", "UFC"],
+    },
+    {
+      id: "6",
+      name: "Conor McGregor",
+      date: "14/07/1988",
+      tags: ["Notorious", "Striking", "Whiskey"],
+    },
+    {
+      id: "7",
+      name: "Anderson Silva",
+      date: "14/04/1975",
+      tags: ["Spider", "Muay Thai", "Legend"],
+    },
+    {
+      id: "8",
+      name: "Georges St-Pierre",
+      date: "19/05/1981",
+      tags: ["GSP", "Wrestling", "GOAT"],
+    },
+    {
+      id: "9",
+      name: "Daniel Cormier",
+      date: "20/03/1979",
+      tags: ["DC", "Wrestling", "Analyst"],
+    },
+    {
+      id: "10",
+      name: "Khabib Nurmagomedov",
+      date: "20/09/1988",
+      tags: ["Eagle", "Sambo", "GOAT"],
+    },
   ]);
 
-  // Prevent default behavior for drag over
+  const [draggingChat, setDraggingChat] = useState<string | null>(null);
+
+  const handleDragStart = (chatId: string) => {
+    setDraggingChat(chatId);
+  };
+
+  const handleDrop = (folderId: string) => {
+    if (draggingChat) {
+      setFolders((prevFolders) =>
+        prevFolders.map((folder) =>
+          folder.id === folderId
+            ? { ...folder, chats: [...folder.chats, draggingChat] }
+            : folder
+        )
+      );
+      setChats((prevChats) =>
+        prevChats.filter((chat) => chat.id !== draggingChat)
+      );
+      setDraggingChat(null);
+    }
+  };
+
   const handleDragOver = (e: React.DragEvent<HTMLDivElement>) => {
     e.preventDefault();
   };
 
-  // Handle adding new folder
   const addNewFolder = () => {
     const newFolderId = (folders.length + 1).toString();
     const newFolderName = `Folder ${newFolderId}`;
     setFolders([
       ...folders,
-      { id: newFolderId, name: newFolderName, files: [] },
+      { id: newFolderId, name: newFolderName, chats: [] },
     ]);
   };
 
@@ -81,10 +170,10 @@ const ChatsPage: React.FC = () => {
             {folders.map((folder) => (
               <div
                 key={folder.id}
-                className="p-4 w-60 bg-gray-100 dark:bg-gray-700 text-gray-700 dark:text-gray-100 rounded-md shadow-lg flex flex-col cursor-pointer justify-between"
+                className="p-4 w-60 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 rounded-md shadow-lg flex flex-col cursor-pointer justify-between"
+                onDrop={() => handleDrop(folder.id)}
                 onDragOver={handleDragOver}
               >
-                {/* Icon and Folder Name in Same Line */}
                 <div className="flex items-center space-x-2">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -103,6 +192,48 @@ const ChatsPage: React.FC = () => {
                   <h2 className="text-lg font-medium truncate w-40">
                     {folder.name}
                   </h2>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        {/* Chats Section */}
+        <div>
+          <div className="text-xl font-bold mb-4 text-gray-800 dark:text-gray-100">
+            All your{" "}
+            <span className="text-indigo-700 dark:text-indigo-500">chats</span>.
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4">
+            {chats.map((chat) => (
+              <div
+                key={chat.id}
+                className="p-4 bg-white dark:bg-gray-700 text-gray-700 dark:text-gray-100 font-medium rounded-md shadow-md cursor-pointer"
+                draggable
+                onDragStart={() => handleDragStart(chat.id)}
+              >
+                <div className="truncate w-full text-lg font-bold">
+                  {chat.name}
+                </div>
+                <div className="font-light text-sm">Last used: {chat.date}</div>
+                <div className="overflow-hidden">
+                  <span className="text-sm font-medium text-gray-400">
+                    Tags:{" "}
+                  </span>
+                  <div className="flex space-x-1 h-8 whitespace-nowrap no-scrollbar overflow-y-hidden">
+                    {chat.tags.length > 0 ? (
+                      chat.tags.map((tag) => (
+                        <span
+                          key={tag}
+                          className="bg-indigo-600 text-white text-xs px-3 py-2 rounded-md"
+                        >
+                          {tag}
+                        </span>
+                      ))
+                    ) : (
+                      <span className="text-xs text-gray-500">-</span>
+                    )}
+                  </div>
                 </div>
               </div>
             ))}
