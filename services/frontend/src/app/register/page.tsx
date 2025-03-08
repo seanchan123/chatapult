8864ee64375b2501b2b9e2f1febdcdb87ae99483
Sean Chan <seanchan@outlook.sg>
2025-03-09 04:30:53 +0800

    Modify `register` API call in `frontend`

--- services/frontend/src/app/register/page.tsx
+++ services/frontend/src/app/register/page.tsx
@@ -24,7 +24,7 @@ const RegisterPage: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/auth/register", {
+      const response = await fetch("http://localhost:4000/api/auth/register", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
