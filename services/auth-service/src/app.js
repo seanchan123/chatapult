07966f50aab5cd6ad1c2ea8ca81225c563f663e2
Sean Chan <seanchan@outlook.sg>
2025-03-09 04:30:35 +0800

    Created `auth-service` to run at PORT `4000`

---
+++ services/auth-service/src/app.js
@@ -0,0 +1,39 @@
+// src/app.js
+import express from 'express';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cors from 'cors';
+
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Use morgan to log requests
+app.use(morgan('combined'));
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Allow requests from frontend
+app.use(cors({
+  origin: "http://localhost:3000",
+  credentials: true,
+}));
+
+// Mount routes
+app.use('/api/auth', authRoutes);
+
+// Global error handler
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => console.log(`Authentication Service running on port ${PORT}`));
---
+++ services/auth-service/src/controllers/authController.js
@@ -0,0 +1,36 @@
+// src/controllers/authController.js
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import User from '../models/User.js';
+
+const JWT_SECRET = process.env.JWT_SECRET || '8baa27b1ed70ddd4b5b4f5f582791c7a6fce2b19a35e7f3d4a5bc2ed6069ad65';
+
+export const register = async (req, res) => {
+  try {
+
+    const { username, email, password } = req.body;
+
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ error: 'User already exists' });
+    }
+
+    // Hash the password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create and save the user
+    const user = new User({ username, email, password: hashedPassword });
+    await user.save();
+
+    // Generate a JWT token
+    const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, { expiresIn: '1d' });
+
+    res.status(201).json({ token });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message });
+  }
+};
