37b23f6f6922826d688d83de57c1533ef402fd1f
Sean Chan <seanchan@outlook.sg>
2025-03-09 06:15:16 +0800

    Replaced `/register` API URL with `.env.local` variable

--- services/frontend/src/app/register/page.tsx
+++ services/frontend/src/app/register/page.tsx
@@ -24,7 +24,7 @@ const RegisterPage: React.FC = () => {
     }
 
     try {
-      const response = await fetch("http://localhost:4000/api/auth/register", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_AUTH_SERVICE_URL}/api/auth/register`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ username, email, password }),
